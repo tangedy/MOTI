b7a7df4d988935dfa15a91318754521230512c42
tangedy <edtangingson@gmail.com>
2025-05-27 14:50:35 -0400

    when in doubt just push it

---
+++ server/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+GROQ_API_KEY = "placeholder"
+GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
+
+
+@app.route("/api/paraphrase", methods=["POST"])
+def paraphrase():
+    data = request.get_json()
+    goal = data.get("goal", "").strip()
+    if goal == '':
+        return "<h1>u didnt submit anything!!</h1>"
+    
+
