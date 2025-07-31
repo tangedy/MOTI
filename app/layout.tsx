a4ce93e3439f8a8b2aa3e07172591f31e22fe0fa
Edward Tang <edtangingson@gmail.com>
2025-07-31 12:58:19 -0400

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,15 @@ export const metadata: Metadata = {
   title: 'MOTI',
   description: '',
   generator: '',
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: '16x16 32x32', type: 'image/x-icon' },
+      { url: '/favicon.ico', sizes: '32x32', type: 'image/x-icon' },
+      { url: '/favicon.png', sizes: '32x32', type: 'image/png' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({
@@ -14,6 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   )
